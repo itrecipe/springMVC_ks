e91ced333afbdf9d9afb29515611e8a434d89d1d
ITRecipe <itrecipe95@gmail.com>
2024-05-08 17:44:31 +0900

    spring_exam
    24.05.08

---
+++ project/ch03_pjt_03/src/main/java/ch03_pjt_03/CalDiv.java
@@ -0,0 +1,11 @@
+package ch03_pjt_03;
+
+public class CalDiv implements ICalculator {
+	
+	@Override
+	public int doOperation(int firstNum, int secondNum) {
+		return secondNum != 0 ? (firstNum / secondNum) : 0;
+	}
+	
+
+}
