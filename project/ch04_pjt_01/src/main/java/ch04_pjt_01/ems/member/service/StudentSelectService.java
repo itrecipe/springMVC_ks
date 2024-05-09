8bd2253208318869c32a5ebf27ab9241e2fb0a62
ITRecipe <itrecipe95@gmail.com>
2024-05-09 17:27:34 +0900

    spring _exam
    24.05.08 학습

---
+++ project/ch04_pjt_01/src/main/java/ch04_pjt_01/ems/member/service/StudentSelectService.java
@@ -0,0 +1,30 @@
+package ch04_pjt_01.ems.member.service;
+
+import ch04_pjt_01.ems.member.Student;
+import ch04_pjt_01.ems.member.dao.StudentDao;
+
+//학생 만명의 정보를 받을 서비스 생성
+public class StudentSelectService {
+
+	private StudentDao studentDao; //studentDao 필드를 통해 정보를 가져온다 
+
+	public StudentSelectService(StudentDao studentDao) {
+		this.studentDao = studentDao;
+	}
+
+	public Student select(String sNum) {
+		if(verify(sNum)) {
+			return studentDao.select(sNum);
+		} else {
+			System.out.println("Student information is available");
+		}
+
+		return null;
+	}
+
+	public boolean verify(String sNum) {
+		Student student = studentDao.select(sNum);
+		return student != null ? true : false;
+
+	}
+}
